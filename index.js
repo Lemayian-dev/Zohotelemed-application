d8329cfe46b2e1de358f3b7b351472ad3e91025c
Isaac Lemayian <105207068+Lemayian-dev@users.noreply.github.com>
2024-09-12 15:10:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navList = document.querySelector('.nav-list');
+
+    hamburgerMenu.addEventListener('click', function() {
+      navList.classList.toggle('active');
+    });
+  });
+</script>
